af455df7cdb94603c3aec43c351d93872d517f1a
adruijter <adruijter@gmail.com>
2024-11-14 14:19:21 +0100

    Initial commit

---
+++ db/createscript.sql
@@ -0,0 +1,14 @@
+-- Step 01
+-- **************************************************************
+-- Doel : Maak een nieuwe database voor het opslaan van achtbanen
+-- **************************************************************
+-- Versie   Datum       Auteur              Omschrijving
+-- ******   *****       ******              ************
+-- 01       14-11-2024  Arjan de Ruijter    Achtbanen van Europa
+-- **************************************************************
+
+-- Verwijder database Achtbaan-2408a
+DROP DATABASE IF EXISTS `Achtbaan-2408a`;
+
+-- Maak een nieuwe database aan Achtbaan-2408a
+CREATE DATABASE `Achtbaan-2408a`;
