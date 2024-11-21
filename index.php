1142b44ba4028a1f6d91265fe1d6887e2bb884d0
adruijter <adruijter@gmail.com>
2024-11-21 14:10:45 +0100

    Testtable done

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hoogste Achtbanen van Europa</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h3>Hoogste Achtbanen van Europa</h3>
+
+
+    <table class="table table-hover">
+        <thead>
+            <th>Naam Achtbaan</th>
+            <th>Naam Pretpark</th>
+            <th>Land</th>
+            <th>Topsnelheid</th>
+            <th>Hoogte</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Test1</td>
+                <td>Test2</td>
+                <td>Test3</td>
+                <td>Test4</td>
+                <td>Test5</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
