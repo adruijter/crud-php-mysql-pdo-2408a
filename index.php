1e71c615288e01b46d4cf4e288085903a8f5de7e
adruijter <adruijter@gmail.com>
2024-11-28 14:19:42 +0100

    Link erbij gedaan

--- index.php
+++ index.php
@@ -56,30 +56,56 @@
     <title>Hoogste Achtbanen van Europa</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
-    <h3>Hoogste Achtbanen van Europa</h3>
+    <div class="container mt-3">
 
-    <table class="table table-hover">
-        <thead>
-            <th>Naam Achtbaan</th>
-            <th>Naam Pretpark</th>
-            <th>Land</th>
-            <th>Topsnelheid</th>
-            <th>Hoogte</th>
-        </thead>
-        <tbody>
-            <?php foreach($result as $achtbaanInfo) : ?>
-                  <tr>
-                     <td><?= $achtbaanInfo->Naam ?></td>
-                     <td><?= $achtbaanInfo->NaamPretpark ?></td>
-                     <td><?= $achtbaanInfo->Land ?></td>
-                     <td><?= $achtbaanInfo->Topsnelheid ?></td>
-                     <td><?= $achtbaanInfo->Hoogte ?></td>                 
-                  </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+      <div class="row mb-1">
+        <div class="col-2"></div>
+        <div class="col-8 text-primary"><h3>Hoogste Achtbanen van Europa</h3></div>
+        <div class="col-2"></div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-2"></div>
+        <div class="col-8">
+          <h5>Nieuwe achtbaan <i class="bi bi-plus-square-fill text-danger"></i></h5>
+        </div>
+        <div class="col-2"></div>
+      </div>
+
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+          <table class="table table-hover">
+              <thead>
+                  <th>Naam Achtbaan</th>
+                  <th>Naam Pretpark</th>
+                  <th>Land</th>
+                  <th>Topsnelheid</th>
+                  <th>Hoogte</th>
+              </thead>
+              <tbody>
+                  <?php foreach($result as $achtbaanInfo) : ?>
+                        <tr>
+                          <td><?= $achtbaanInfo->Naam ?></td>
+                          <td><?= $achtbaanInfo->NaamPretpark ?></td>
+                          <td><?= $achtbaanInfo->Land ?></td>
+                          <td><?= $achtbaanInfo->Topsnelheid ?></td>
+                          <td><?= $achtbaanInfo->Hoogte ?></td>                 
+                        </tr>
+                  <?php endforeach ?>
+              </tbody>
+          </table>
+        </div>        
+        <div class="col-2"></div>
+      </div>
+
+      
+
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
